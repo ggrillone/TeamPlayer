20fc08b5c0f8e88595923a4c0fbc97f53f6bedcc
Gregory Grillone <greg.grillone@gmail.com>
2012-07-12 15:29:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamPlayer::Application.config.secret_token = '6f104f40b269b3be5ab7758b9b8a6aafff4cdafe2b14c8edc12c0b8b483a41dbdd990dab29f616c5e09b3bac326c6af6ccf2db43198c70f79af0dbd7dc2ddc6f'
