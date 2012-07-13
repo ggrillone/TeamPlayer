d56c72033e55218b5a4a55183ec6def0d4d2cabd
Gregory Grillone <greg.grillone@gmail.com>
2012-07-13 13:44:19 -0400

    Make basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+  has_secure_password
+
+  # To help validate uniqueness on the database side
+  before_save { |user| user.email = email.downcase }
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+  					uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
